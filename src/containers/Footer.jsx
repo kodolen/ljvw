4327d363a0164bce9ba5f13176a1e29095c4e049
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2020-03-10 17:01:54 +0100

    Added footer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,11 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import FB from '../assets/Facebook.png';
+import Insta from '../assets/Instagram.png';
+import LinkedIN from '../assets/LinkedIN.png';
+import Mail from '../assets/Mail.png';
+import Github from '../assets/Github.png';
 
-class HeadBanner extends Component {
+
+class Footer extends Component {
     render() {
         return (
-            <h1>Footer</h1>
+            <div className="footer">
+                <div className="row">
+                    <div className="col-sm-5 col-xs-12">
+                        <h3>LJVW</h3>
+                        <p className="footer-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="site-index">
+                            <span className="index-name">HOME</span>
+                            <span className="index-name">OVER MIJ</span>
+                            <span className="index-name">PORTFOLIO</span>
+                            <span className="index-name">CONTACT</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-5 off-sm-1 col-xs-12">
+                        <h3>Kanalen</h3>
+                        <p className="footer-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="site-index">
+                            <span className="index-name"><a href="#"><img src={Mail} alt="" /></a></span>
+                            <span className="index-name"><a href="#"><img src={LinkedIN} alt="" /></a></span>
+                            <span className="index-name"><a href="#"><img src={Insta} alt="" /></a></span>
+                            <span className="index-name"><a href="#"><img src={FB} alt="" /></a></span>
+                            <span className="index-name"><a href="#"><img src={Github} alt="" /></a></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <span className="footer-bottom-text">2020 LJVW Development</span>
+                    </div>
+                </div>
+            </div>
         )
     };
 }
 
-export default HeadBanner;
+export default Footer;
