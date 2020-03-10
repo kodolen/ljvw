89df911d7aecc0389dc6b38e342892ffcfc8d523
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2020-03-10 15:01:07 +0100

    Added landingspage

--- src/containers/Landings.jsx
+++ src/containers/Landings.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import BG from '../assets/background-len.jpg';
 
 class Landings extends Component {
     render() {
         return (
             <div className="landings">
-                <div className="side-info">
+                <div className="side-text-wrapper">
                     <span className="side-text">LENNART JOHANNES VAN WELZEN</span>
+                </div>
+                <div className="foreground"></div>
+                <div className="background">
+                    <img src={BG} alt="" />
                 </div>
                 <div className="inner">
                     <div className="row">
-                        <div className="col-md-6 off-md-1">
+                        <div className="col-xs-10 col-sm-6 off-sm-1">
                             <div className="intro">
                                 <div className="intro-content">
                                     <span className="thin-title">Freelance</span>
@@ -18,7 +23,7 @@ class Landings extends Component {
                                 </div>
                                 <div className="button-wrapper">
                                     <div className="button primary"><span>Contact</span></div>
-                                    <div className="button secondary"><span>Bekijk portfolio</span></div>
+                                    <div className="button secondary"><span>Portfolio</span></div>
                                 </div>
                             </div>
                         </div>
