c4ffb9bfcd51b0ee1eb6f62bcd7950b8835d7229
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2020-03-10 16:14:13 +0100

    Added contact

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,11 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class HeadBanner extends Component {
+class Contact extends Component {
     render() {
         return (
-            <h1>Contact</h1>
+            <div className="row">
+                <div className="col-xs-12 col-md-6 off-md-3">
+                    <div className="contact">
+                        <p className="hl-text">Bent u klaar om uw bedrijf nieuw leven in te blazen? Ik ga die <span className="bold">uitdaging</span> graag met u aan!</p>
+                        <p className="hl-subtext">U kunt contact op nemen via: <a href="mailto:info@ljvw.nl">info@ljvw.nl</a></p>
+                    </div>
+                </div>
+            </div>
         )
     };
 }
 
-export default HeadBanner;
+export default Contact;
