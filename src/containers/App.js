326bbc60f68ed09a408c8dc595c61c64cfd8ecfe
Lennart van Welzen <34541619+kodolen@users.noreply.github.com>
2020-03-19 12:57:42 +0100

    Added changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,6 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <IpTracker></IpTracker>
       <Navigation></Navigation>
       <Landings></Landings>
       <div className="inner">
